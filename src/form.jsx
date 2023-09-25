3725b68d7c24fbc5fff81c748ef9a681a791f117
davislucky <58579323+davislucky@users.noreply.github.com>
2023-09-25 15:15:33 -0700

    Add error messages below input fields

--- src/form.jsx
+++ src/form.jsx
@@ -12,34 +12,47 @@ function Form() {
         notifications: false
     });
 
-    const [errors, setErrors] = useState([]);
+    const [errors, setErrors] = useState({});
 
     const validate = () => {
-        let errors = [];
+        let errors = {};
         if(user.name.length === 0){
-            errors.push("Name can't be blank");
+            if (!errors["name"]) {
+                errors["name"] = [];
+            } 
+            errors["name"].push("Name can't be blank"); 
         }
 
         if (user.email.length === 0){
-            errors.push("Email can't be blank");
-        }
-
-        let [fp, domain] = user.email.split("@")
-        console.log(fp, domain);
-        if (!fp || !domain || !domain.includes(".com")){
-            errors.push("Invalid email")
+            if (!errors["email"]) {
+                errors["email"] = [];
+            } 
+            errors["email"].push("Email can't be blank"); 
+        } else {
+            let [fp, domain] = user.email.split("@")
+            if (!fp || !domain || !domain.includes(".com")){
+                if (!errors["email"]) {
+                    errors["email"] = [];
+                } 
+                errors["email"].push("Invalid email"); 
+            }
         }
 
         if (user.phoneNumber){
             let [areaCode, middle, last] = user.phoneNumber.split("-")
-            console.log(areaCode, middle, last);
             if ((!areaCode || !middle || !last) || (areaCode.length !== 3 || middle.length !== 3 || last.length !== 4)){
-                errors.push("Invalid phoneNumber")
+                if (!errors["phoneNumber"]) {
+                    errors["phoneNumber"] = [];
+                } 
+                errors["phoneNumber"].push("Invalid phone number"); 
             }
         }
 
         if (user.bio.length > 280){
-            errors.push("Invalid bio")
+            if (!errors["bio"]) {
+                errors["bio"] = [];
+            } 
+            errors["bio"].push("Bio must be less than 280 characters"); 
         }
 
         return errors;
@@ -52,18 +65,17 @@ function Form() {
 
     return (
         <>
-            {errors.map(error => (
-                <div>{error}</div>
-            ))}
-
             <form onSubmit={handleSubmit}>
                 <input type="text" placeholder="Name"  value={user.name} onChange={(event) => setUser({...user, name: event.currentTarget.value})}/>
+                <div className="errors">{errors["name"]}</div>
                 <br></br>
                 <br></br>
                 <input type="text" placeholder="Email" value={user.email} onChange={(event) => setUser({...user, email: event.currentTarget.value})} />
+                <div className="errors">{errors["email"]}</div>
                 <br></br>
                 <br />
                 <input type="text" placeholder="Phone Number" value={user.phoneNumber} onChange={(event) => setUser({...user, phoneNumber: event.currentTarget.value})} />
+                <div className="errors">{errors["phoneNumber"]}</div>
                 <br></br>
                 <br></br>
                 <label for="phone-type"> PhoneType: </label>
@@ -73,8 +85,7 @@ function Form() {
                     <option value="work" selected={user.phoneType === "work" ? "selected": ""}>Work</option>
                     <option value="mobile" selected={user.phoneType === "mobile" ? "selected": ""}>Mobile</option>
                 </select>
-                <div>Staff :  
-
+                <div>Staff  :  
                     <label>Instructor
                         <input name="staff" type="radio" value="Instructor" 
                         checked={user.staff === "Instructor" ? "checked": ""} onChange={(event) => setUser({...user, staff: event.currentTarget.value})}></input>
@@ -88,6 +99,7 @@ function Form() {
                 <textarea placeholder="Bio" rows="4" cols="15" value={user.bio} 
                 onChange={(event) => setUser({...user, bio: event.currentTarget.value})}/>
                 <br />
+                <div className="errors">{errors["bio"]}</div>
                 <label>Sign up for email notifications!
                     <input type="checkbox" name="email-sign-up-notifications"  checked={user.notifications === true ? "checked": ""} onChange={(event) => {
                         let val = event.currentTarget.checked ? true: false;
